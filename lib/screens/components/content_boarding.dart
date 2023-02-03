90ae699aae1974bd470bad2383566f468dc8cfa8
JorgeCamToledo <camacho.ja0402@gmail.com>
2023-02-02 18:50:17 -0600

    Onboardimg edited an pagination added

--- lib/screens/components/content_boarding.dart
+++ lib/screens/components/content_boarding.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 
 class ContentBoarding extends StatelessWidget {
-  const ContentBoarding({super.key, this.text, this.image, this.descripcion});
+  const ContentBoarding(
+      {super.key, this.text, this.image, this.descripcion, this.descripcion2});
   final String? text;
   final String? image;
   final String? descripcion;
+  final String? descripcion2;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 60.0),
-          child: Text(
-            text!,
-            style: const TextStyle(fontSize: 40),
+          padding: const EdgeInsets.only(top: 250.0),
+          child: Image.asset(
+            image!,
+            height: 220,
           ),
         ),
-        Image.asset(
-          image!,
-          height: 100,
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Text(
+            text!,
+            style: const TextStyle(
+                fontSize: 25, color: Color.fromARGB(255, 110, 26, 124)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Text(
+            descripcion!,
+            style: const TextStyle(
+                fontSize: 17, color: Color.fromARGB(255, 155, 159, 161)),
+          ),
         ),
         Text(
-          descripcion!,
-          style: const TextStyle(fontSize: 20),
-        )
+          descripcion2!,
+          style: const TextStyle(
+              fontSize: 17, color: Color.fromARGB(255, 155, 159, 161)),
+        ),
       ],
     );
   }
--- lib/screens/components/on_boarding.dart
+++ lib/screens/components/on_boarding.dart
@@ -1,6 +1,8 @@
 import 'package:first_app/screens/components/content_boarding.dart';
 import 'package:flutter/material.dart';
 
+import '../../pages/login.dart';
+
 class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
@@ -9,22 +11,51 @@ class OnBoarding extends StatefulWidget {
 }
 
 class _OnBoardingState extends State<OnBoarding> {
-  int currentePage = 0;
+  int currentPage = 0;
+  late PageController varController;
+
+  @override
+  void initState() {
+    super.initState();
+    varController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    varController.dispose();
+  }
+
   List<Map<String, String>> listBoarding = [
     {
-      "titulo": "BMO",
-      "imagen": "assets/images/bmo.jpg",
-      "descripcion": "ese es bmo"
+      "titulo": "ESPARCIMIENTO",
+      "imagen": "assets/images/B1.png",
+      "descripcion": "brindamos todos los servicios para ",
+      "descripcion2": "consentir a tu mascota",
     },
     {
-      "titulo": "Jake",
-      "imagen": "assets/images/jake.jfif",
-      "descripcion": "ese es jake"
+      "titulo": "ADOPCION",
+      "imagen": "assets/images/B2.png",
+      "descripcion": "nulla faucibus tellus ut odio scelerisque, ",
+      "descripcion2": "vitae molestie lectus feugiat",
     },
     {
-      "titulo": "El Chueck",
-      "imagen": "assets/images/shrek.jfif",
-      "descripcion": "shrek"
+      "titulo": "HOSPITALIDAD",
+      "imagen": "assets/images/B3.png",
+      "descripcion": "nulla faucibus tellus ut odio scelerisque,",
+      "descripcion2": "vitae molestie lectus feugiat",
+    },
+    {
+      "titulo": "VETERINARIA",
+      "imagen": "assets/images/B4.png",
+      "descripcion": "nulla faucibus tellus ut odio scelerisque,",
+      "descripcion2": "vitae molestie lectus feugiat",
+    },
+    {
+      "titulo": "TIENDA",
+      "imagen": "assets/images/B5.png",
+      "descripcion": "Compra todas las necesidades de tu ",
+      "descripcion2": "mascota sin salir de casa",
     },
   ];
 
@@ -33,28 +64,88 @@ class _OnBoardingState extends State<OnBoarding> {
     return SafeArea(
       child: Column(
         children: [
-          Expanded(
-            flex: 2,
+          SizedBox(
+            width: double.infinity,
+            height: 600,
             child: PageView.builder(
+              controller: varController,
+              onPageChanged: (value) {
+                setState(() {
+                  currentPage = value;
+                });
+              },
               itemBuilder: (context, index) => ContentBoarding(
                 text: listBoarding[index]['titulo'],
                 image: listBoarding[index]['imagen'],
                 descripcion: listBoarding[index]['descripcion'],
+                descripcion2: listBoarding[index]['descripcion2'],
               ),
               itemCount: listBoarding.length,
             ),
           ),
-          const Expanded(
-            flex: 2,
-            child: Center(
-              child: OutlinedButton(
-                onPressed: null,
-                child: Text('Button'),
-              ),
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                      listBoarding.length,
+                      (index) =>
+                          pages(index: index, currentePage: currentPage)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 145),
+                  child: SizedBox(
+                    height: 50,
+                    width: 300,
+                    child: MaterialButton(
+                      color: currentPage == listBoarding.length - 1
+                          ? Colors.green
+                          : Colors.white,
+                      onPressed: () async {
+                        currentPage == listBoarding.length - 1
+                            ? Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => const Login()))
+                            : varController.nextPage(
+                                duration: const Duration(milliseconds: 500),
+                                curve: Curves.easeInOut);
+                      },
+                      textColor: currentPage == listBoarding.length - 1
+                          ? Colors.white
+                          : Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          side: const BorderSide(width: 1, color: Colors.grey),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text(
+                          currentPage == listBoarding.length - 1
+                              ? 'Continuar'
+                              : "Siguiente",
+                          style: const TextStyle(fontSize: 16)),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
       ),
     );
   }
+}
+
+AnimatedContainer pages({required int index, required int currentePage}) {
+  return AnimatedContainer(
+    margin: EdgeInsets.only(right: 5),
+    width: currentePage == index ? 25 : 15,
+    height: 5,
+    duration: kThemeAnimationDuration,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: currentePage == index
+            ? Colors.pink
+            : Color.fromARGB(255, 175, 171, 171)),
+  );
 }
